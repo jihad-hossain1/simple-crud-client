37dc917b1806d5c9dda1157db58c3318cac5f24c
jihad-hossain1 <jihadkham934@gmail.com>
2023-05-08 13:02:13 +0600

    user added on mongodb

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import './App.css'
+
+function App() {
+
+  
+  const handleUser = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email }
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(user)
+    }).then(res => res.json())
+      .then(data => {
+      console.log(data);
+    })
+
+}
+
+  return (
+    <>
+      <h2>Simple CRUD</h2>
+      <form onSubmit={handleUser}>
+        <input type="text" name='name' /> <br />
+        <input type="email" name='email' /> <br />
+        <input type="submit" value='add user' /> <br />
+      </form>
+    </>
+  )
+}
+
+export default App
