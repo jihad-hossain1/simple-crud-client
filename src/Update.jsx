f030ab854d975cb8a308dea2ff1c7217c4903b3b
jihad-hossain1 <jihadkhan934@gmail.com>
2023-05-09 17:52:00 +0600

    update user by PUT method in client side

--- src/Update.jsx
+++ src/Update.jsx
@@ -3,10 +3,44 @@ import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
   const loadedData = useLoaderData();
-
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name, email);
+    const updatedUser = { name, email };
+    fetch(`http://localhost:5000/users/${loadedData._id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount) {
+          alert("User updated successfully");
+          form.reset();
+        }
+      });
+  };
   return (
     <div>
       <h2>Update information of : {loadedData.name}</h2>
+      <div>
+        <form onSubmit={handleUpdate}>
+          <input type="text" name="name" defaultValue={loadedData?.name} />
+          <br />
+          <input
+            type="email"
+            name="email"
+            id=""
+            defaultValue={loadedData?.email}
+          />
+          <br />
+          <input type="submit" value="update" />
+        </form>
+      </div>
     </div>
   );
 };
